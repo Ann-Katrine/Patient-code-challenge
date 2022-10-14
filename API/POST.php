79a04817f85b8fbe3c1099ad0b24d2df5b016cac
Ann-Katrine Nykjær <33825997+Ann-Katrine@users.noreply.github.com>
2022-10-14 12:33:28 +0200

    standart opsætning til  CRUD

---
+++ API/POST.php
@@ -0,0 +1,67 @@
+<?php
+    // gør at man for alle fejl frem 
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+    include_once("./validering.php");
+    include_once("./ORM/Repository/AdmissionRepository.php");
+    include_once("./ORM/Repository/DepartmentRepository.php");
+    include_once("./ORM/Repository/DoctorRepository.php");
+    include_once("./ORM/Repository/medicalJournal.php");
+
+    class POST{
+        private $arrayVali = array();
+        private $countUri = 0;
+        private $header;
+
+        public function findRoute($uri, $headers)
+        {
+            
+            if(in_array("application/json", $headers)){
+
+                switch ($uri[1]) {
+                    case 'admission':
+                        $this->admissionRoute($uri);
+                        break;
+                    case 'docter':
+                        $this->docterRoute($uri);
+                        break;
+                    case 'department':
+                        $this->departmentRoute($uri);
+                        break;
+                    case 'medical-journal':
+                        $this->medicalJournal($uri);
+                        break;
+                    default:
+                        http_response_code(400);
+                        die("400 - bad request method!");
+                        break;
+                }
+            }
+            else{
+                http_response_code(412);
+				die('412 - Wrong Accept type. Only JSON supported!');
+            }
+        }
+
+        private function admissionRoute($uri)
+        {
+            # code...
+        }
+
+        private function docterRoute($uri)
+        {
+            # code...
+        }
+
+        private function departmentRoute($uri)
+        {
+            # code...
+        }
+
+        private function medicalJournal($uri)
+        {
+            # code...
+        }
+    }
+?>
